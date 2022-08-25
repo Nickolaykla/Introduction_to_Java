e1dd3bb634a4681f82cb3788815fa16f33212e85
Nickolaykla <nickolaykla@gmail.com>
2022-08-25 18:29:24 +0300

    added 2 task of branching

--- src/main/java/basics_of_software_code_development/branching/_2/Solution.java
+++ src/main/java/basics_of_software_code_development/branching/_2/Solution.java
@@ -1,4 +1,25 @@
 package basics_of_software_code_development.branching._2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// Найти max{min(a,b),min(c,d)}
+
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            double a = Double.parseDouble(reader.readLine());
+            double b = Double.parseDouble(reader.readLine());
+            double c = Double.parseDouble(reader.readLine());
+            double d = Double.parseDouble(reader.readLine());
+
+            double result = findMax(a, b, c, d);
+            System.out.println(result);
+        }
+    }
+
+    public static double findMax(double a, double b, double c, double d) {
+        return Math.max(Math.min(a, b), Math.min(c, d));
+    }
 }
