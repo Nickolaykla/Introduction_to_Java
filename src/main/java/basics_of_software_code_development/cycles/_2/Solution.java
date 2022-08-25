6f4c6370931d4e8b7c18dbfdff25025e5d11306a
Nickolaykla <nickolaykla@gmail.com>
2022-08-25 23:02:11 +0300

    added 3 task cycle

--- src/main/java/basics_of_software_code_development/cycles/_2/Solution.java
+++ src/main/java/basics_of_software_code_development/cycles/_2/Solution.java
@@ -1,4 +1,37 @@
 package basics_of_software_code_development.cycles._2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* Вычислить значения функции на отрезке [a,b] с шагом h
+ * */
+
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            double a = Double.parseDouble(reader.readLine());
+            double b = Double.parseDouble(reader.readLine());
+            double h = Double.parseDouble(reader.readLine());
+
+            function(a, b, h);
+        }
+    }
+
+    public static void function(double a, double b, double h) {
+        double x = a;
+        double y;
+
+        while (x <= b) {
+            if (x <= 2) {
+                y = -x;
+
+            } else {
+                y = x;
+            }
+            System.out.println("x = " + x + "     y = " + y);
+
+            x += h;
+        }
+    }
 }
