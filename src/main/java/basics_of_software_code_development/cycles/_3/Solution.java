23ffe7db26eb654330423d7bad245442871e5680
Nickolaykla <nickolaykla@gmail.com>
2022-08-25 23:26:04 +0300

    Added 3 and 4 task of cycle

--- src/main/java/basics_of_software_code_development/cycles/_3/Solution.java
+++ src/main/java/basics_of_software_code_development/cycles/_3/Solution.java
@@ -1,7 +1,17 @@
 package basics_of_software_code_development.cycles._3;
 
+// Найти сумму квадратов первых 100 чисел.
+
 public class Solution {
     public static void main(String[] args) {
+        System.out.println(sumOfSquares(100));
+    }
 
+    public static long sumOfSquares(int num) {
+        long sum = 0;
+        for (int i = 1; i <= num ; i++) {
+            sum += Math.pow(i, 2);
+        }
+        return sum;
     }
 }
--- src/main/java/basics_of_software_code_development/cycles/_4/Solution.java
+++ src/main/java/basics_of_software_code_development/cycles/_4/Solution.java
@@ -1,4 +1,17 @@
 package basics_of_software_code_development.cycles._4;
 
+import java.math.BigInteger;
+
+// Составить программу нахождения квадратов первых 200 чисел
+
 public class Solution {
+    public static void main(String[] args) {
+
+        BigInteger bigInteger = BigInteger.ONE;
+        for (int i = 1; i <= 200; i++) {
+            bigInteger = bigInteger.multiply(BigInteger.valueOf(i * i));
+        }
+
+        System.out.println(bigInteger);
+    }
 }
