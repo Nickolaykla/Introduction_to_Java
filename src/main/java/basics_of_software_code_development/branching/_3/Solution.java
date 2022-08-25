74517f418e336ab996a22e33a22740b0720a00cb
Nickolaykla <nickolaykla@gmail.com>
2022-08-25 19:01:49 +0300

    4 task of branching added

--- src/main/java/basics_of_software_code_development/branching/_3/Solution.java
+++ src/main/java/basics_of_software_code_development/branching/_3/Solution.java
@@ -1,4 +1,28 @@
 package basics_of_software_code_development.branching._3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+Даны три точки А(х1, у1), B(x2, y2), C(x3, y3). Определить, будут ли они расположены на одной
+прямой.
+*/
+
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            double x1 = Double.parseDouble(reader.readLine());
+            double y1 = Double.parseDouble(reader.readLine());
+            double x2 = Double.parseDouble(reader.readLine());
+            double y2 = Double.parseDouble(reader.readLine());
+            double x3 = Double.parseDouble(reader.readLine());
+            double y3 = Double.parseDouble(reader.readLine());
+
+            System.out.println(isOnLine(x1, y1, x2, y2, x3, y3));
+        }
+    }
+    public static boolean isOnLine(double x1, double y1, double x2, double y2, double x3, double y3) {
+        return ((x3 - x1) / (x2 - x1)) == ((y3 - y1) / (y2 - y1));
+    }
 }
--- src/main/java/basics_of_software_code_development/branching/_4/Solution.java
+++ src/main/java/basics_of_software_code_development/branching/_4/Solution.java
@@ -1,4 +1,25 @@
 package basics_of_software_code_development.branching._4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* Заданы размеры А, В прямоугольного отверстия и размеры x, y, z кирпича. Определить,
+пройдет ли кирпич через отверстие
+*/
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            double A = Double.parseDouble(reader.readLine()); // высота отверстия
+            double B = Double.parseDouble(reader.readLine()); // ширина отверстия
+            double x = Double.parseDouble(reader.readLine()); // высота кирпича
+            double y = Double.parseDouble(reader.readLine()); // ширина кирпича
+            double z = Double.parseDouble(reader.readLine()); // длина кирпича
+
+            System.out.println(willPass(A, B, x, y, z));
+        }
+    }
+    public static boolean willPass(double A, double B, double x, double y, double z) {
+        return (x <= A && y <= B) || (x <= B && y <= A);
+    }
 }
