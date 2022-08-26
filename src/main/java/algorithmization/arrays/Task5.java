128578be6e294d37ae747d2e1dfdbd2d421e428a
Nickolaykla <nickolaykla@gmail.com>
2022-08-26 17:23:09 +0300

    added 5 task of arrays

--- src/main/java/algorithmization/arrays/Task5.java
+++ src/main/java/algorithmization/arrays/Task5.java
@@ -1,7 +1,36 @@
 package algorithmization.arrays;
 
-public class Task5 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+// Даны целые числа а1, а2... а_n. Вывести на печать только те числа, для которых a_i > i.
+
+public class Task5 {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Введите длину массива: ");
+            int n = Integer.parseInt(reader.readLine());
+            if (n < 1) return;
+
+            int[] arr = new int[n];
+
+            System.out.println("Введите элементы массива: ");
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = Integer.parseInt(reader.readLine());
+            }
+
+            System.out.println("Введите число-ключ: ");
+            int i = Integer.parseInt(reader.readLine());
+
+            function(arr, i);
+        }
+    }
+
+    public static void function(int[] arr, int i) {
+        System.out.println("Числа, которые больше " + i + ":");
+        for (int num : arr) {
+            if (num > i) System.out.print(num + " ");
+        }
     }
 }
