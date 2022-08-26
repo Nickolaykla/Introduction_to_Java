3b5457587901b66a5e811eef13f323c5769134f0
Nickolaykla <nickolaykla@gmail.com>
2022-08-26 15:40:54 +0300

    adding 8 task of cycle, need to review

--- src/main/java/basics_of_software_code_development/cycles/_8/Solution.java
+++ src/main/java/basics_of_software_code_development/cycles/_8/Solution.java
@@ -1,7 +1,28 @@
 package basics_of_software_code_development.cycles._8;
 
+/*
+Даны два числа. Определить цифры, входящие в запись как первого, так и второго числа.
+*/
+
+import java.util.*;
+
 public class Solution {
     public static void main(String[] args) {
+        sameDigits(543789798, 432423123);
+    }
+    public static void sameDigits(int num1, int num2) { //123 , 461
+        char[] number1 = String.valueOf(num1).toCharArray();
+        char[] number2 = String.valueOf(num2).toCharArray();
 
+        Set<Character> set = new HashSet<>();
+
+        for (char value1 : number1) {
+            for (char value2 : number2) {
+                if (value1 == value2)
+                    set.add(value1);
+            }
+        }
+
+        System.out.println(set);
     }
 }
