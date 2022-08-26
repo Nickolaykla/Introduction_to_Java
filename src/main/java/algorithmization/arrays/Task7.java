072c70483536af8eff7d7cd1aa7404864541660d
Nickolaykla <nickolaykla@gmail.com>
2022-08-26 18:07:32 +0300

    added 7 task of arrays

--- src/main/java/algorithmization/arrays/Task7.java
+++ src/main/java/algorithmization/arrays/Task7.java
@@ -1,7 +1,35 @@
 package algorithmization.arrays;
 
-public class Task7 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+// Даны действительные числа а1, а2 ... а_n. Найти max(d1+a_2n, a_2 + a_2n-1..., a_n + a_n+1)
+
+public class Task7 {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Введите длину массива: ");
+            int n = Integer.parseInt(reader.readLine());
+            if (n < 1) return;
+            double[] arr = new double[n];
+
+            System.out.println("Введите элементы массива: ");
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = Double.parseDouble(reader.readLine());
+            }
+            System.out.println(maxValue(arr));
+        }
+    }
+
+    public static double maxValue(double[] arr) {
+        double maxVal = Double.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            double testVal = arr[i] + arr[arr.length - 1 - i];
+            if (testVal > maxVal) {
+                maxVal = testVal;
+            }
+        }
+        return maxVal;
     }
 }
