447133fc7965d4f1dc7cbab32895cc59c77e7670
Nickolaykla <nickolaykla@gmail.com>
2022-08-28 17:31:42 +0300

    added 5 task of matrix

---
+++ src/main/java/algorithmization/multidimensional_arrays/Task4.java
@@ -0,0 +1,48 @@
+package algorithmization.multidimensional_arrays;
+
+import java.util.Scanner;
+/*
+Сформировать квадратную матрицу порядка n по заданному образцу (n - четное)
+        1   2   3   ... n
+        n  n-1 n-2  ... 1
+        1   2   3   ... n
+        n  n-1 n-2  ... 1
+        .................
+        n  n-1 n-2  ... 1
+*/
+public class Task4 {
+    public static void main(String[] args) {
+        printInitialArray(initArray());
+    }
+    public static int[][] initArray() {
+        try(Scanner in = new Scanner(System.in)) {
+            System.out.println("Введите количество строк: ");
+            int n = in.nextInt();
+
+            System.out.println("Введите количество столбцов: ");
+            int k = in.nextInt();
+
+            if (n != k || n % 2 != 0) throw new IllegalArgumentException();
+
+            int[][] arr = new int[n][k];
+            int number = 1;
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr.length; j++) {
+                    if((i % 2) == 0) {
+                        arr[i][j] = number++;
+                    } else arr[i][j] = --number;
+                }
+            }
+            return arr;
+        }
+    }
+    public static void printInitialArray(int[][] arr) {
+        System.out.println("Итоговый массив: ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/algorithmization/multidimensional_arrays/Task5.java
@@ -0,0 +1,47 @@
+package algorithmization.multidimensional_arrays;
+
+import java.util.Scanner;
+
+/* Сформировать квадратную матрицу порядка n по заданному образцу (n-четное):
+        1   1   1 ... 1 1 1
+        2   2   2 ... 2 2 0
+        3   3   3 ... 3 0 0
+        ...................
+        n-1 n-1 0 ... 0 0 0
+        n   0   0 ... 0 0 0
+* */
+
+public class Task5 {
+    public static void main(String[] args) {
+        printInitialArray(initArray());
+    }
+    public static void printInitialArray(int[][] arr) {
+        System.out.println("Итоговый массив: ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static int[][] initArray() {
+        try(Scanner in = new Scanner(System.in)) {
+            System.out.println("Введите количество строк, столбцов: ");
+            int n = in.nextInt();
+
+            if (n <= 0 || n % 2 != 0) throw new IllegalArgumentException();
+
+            int[][] arr = new int[n][n];
+            int number = 1;
+            int len = arr.length;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < len; j++) {
+                    arr[i][j] = number;
+                }
+                number++;
+                len--;
+            }
+            return arr;
+        }
+    }
+}
