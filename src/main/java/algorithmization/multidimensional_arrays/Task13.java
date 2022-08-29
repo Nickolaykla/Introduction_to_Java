6cc8d3cbbeac5c1f7c12309430b002abd9e8e3b0
Nickolaykla <nickolaykla@gmail.com>
2022-08-29 13:51:40 +0300

    added 13 task matrix

--- src/main/java/algorithmization/multidimensional_arrays/Task13.java
+++ src/main/java/algorithmization/multidimensional_arrays/Task13.java
@@ -1,13 +1,79 @@
 package algorithmization.multidimensional_arrays;
 
+/*
+Отсортировать столбцы матрицы по возрастанию и убыванию элементов
+* */
+
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Scanner;
 
 public class Task13 {
     public static void main(String[] args) {
-        int[]arr = {1,6,4,2,3};
-        Arrays.stream(arr).boxed()
-                .sorted(Comparator.reverseOrder())
-                .toArray();
+        int[][] arr = initialArray();
+        System.out.println("Исходный массив: ");
+        printArray(arr);
+        System.out.println();
+
+        System.out.println("Колонки отсортированы по возрастанию: ");
+        naturalOrderColumnsSort(arr);
+
+        System.out.println("Колонки отсортированы по убыванию: ");
+        reverseOrderColumnsSort(arr);
+    }
+    // Создание матрицы и заполнение случайными значениями
+    public static int[][] initialArray() {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Введите размерность матрицы: ");
+            int n = in.nextInt();
+            int k = in.nextInt();
+            if (n <= 0 || k <= 0) {
+                System.out.println("Некорректный размер");
+                throw new IllegalArgumentException();
+            }
+            int[][] arr = new int[n][k];
+
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    arr[i][j] = (int) (Math.random() * 100 - 50);
+                }
+            }
+            return arr;
+        }
+    }
+    public static void naturalOrderColumnsSort(int[][] arr) {
+        for (int i = 0; i < arr[0].length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                for (int k = 0; k < arr.length - j - 1; k++) {
+                    if (arr[k][i] > arr[k + 1][i]) {
+                        int temp = arr[k][i];
+                        arr[k][i] = arr[k + 1][i];
+                        arr[k + 1][i] = temp;
+                    }
+                }
+            }
+        }
+        printArray(arr);
+    }
+    public static void reverseOrderColumnsSort(int[][] arr) {
+        for (int i = 0; i < arr[0].length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                for (int k = 0; k < arr.length - j - 1; k++) {
+                    if (arr[k][i] < arr[k + 1][i]) {
+                        int temp = arr[k][i];
+                        arr[k][i] = arr[k + 1][i];
+                        arr[k + 1][i] = temp;
+                    }
+                }
+            }
+        }
+        printArray(arr);
+    }
+    public static void printArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
