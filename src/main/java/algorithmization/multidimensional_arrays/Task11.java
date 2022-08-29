b7449e1b89a964a2c588f41edafbeb5bc3f9a235
Nickolaykla <nickolaykla@gmail.com>
2022-08-29 11:09:24 +0300

    added 11 task matrix

---
+++ src/main/java/algorithmization/multidimensional_arrays/Task11.java
@@ -0,0 +1,43 @@
+package algorithmization.multidimensional_arrays;
+/* Матрицу 10х20 заполнить случайными числами от 0 до 15. Вывести на экран саму матрицу и номера
+строк, в которых число 5 встречается три и более раз.
+* */
+public class Task11 {
+    public static void main(String[] args) {
+        int[][] arr = initArray();
+        printArray(arr);
+        checkFrequency5(arr);
+    }
+
+    public static int[][] initArray() {
+        int[][] array = new int[10][20];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = (int) (Math.random() * 15);
+            }
+        }
+        return array;
+    }
+
+    public static void printArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+    public static void checkFrequency5(int[][] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if(arr[i][j] == 5) count++;
+            }
+            if (count >= 3) {
+                System.out.println("Номер строки, в которой 5 встречается 3 и более раз = " + (i+1));
+            }
+            count = 0;
+        }
+    }
+}
