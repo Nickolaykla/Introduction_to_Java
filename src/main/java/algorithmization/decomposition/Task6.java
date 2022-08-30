d0e4d509666d909d30fe91f5615aac0ac2ffa9d3
Nickolaykla <nickolaykla@gmail.com>
2022-08-30 22:15:38 +0300

    added 6 task decomp

---
+++ src/main/java/algorithmization/decomposition/Task6.java
@@ -0,0 +1,28 @@
+package algorithmization.decomposition;
+
+import java.util.Scanner;
+
+/*
+Написать метод(методы), проверяющий, являются ли данные три числа взаимно простыми.
+* */
+public class Task6 {
+    public static void main(String[] args) {
+        try(Scanner in = new Scanner(System.in)) {
+            System.out.println("Введите 3 числа: ");
+            int num1 = in.nextInt();
+            int num2 = in.nextInt();
+            int num3 = in.nextInt();
+
+            System.out.println("Являются ли числа " + num1 + " " + num2 + " " + num3 + " взаимно простыми?");
+            System.out.println(check3Numbers(num1, num2, num3));
+        }
+    }
+
+    public static boolean check3Numbers(int num1, int num2, int num3) {
+        return gcd(gcd(num1, num2), num3) == 1;
+    }
+
+    public static long gcd(long num1, long num2) {
+        return num2 == 0 ? num1 : gcd(num2, num1 % num2);
+    }
+}
