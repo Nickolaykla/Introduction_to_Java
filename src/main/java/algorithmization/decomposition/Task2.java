40d23eee98ad018e4cfd3efe2131500bd7be1b58
Nickolaykla <nickolaykla@gmail.com>
2022-08-30 19:52:30 +0300

    task2 decomp added

---
+++ src/main/java/algorithmization/decomposition/Task2.java
@@ -0,0 +1,27 @@
+package algorithmization.decomposition;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Введите 4 числа: ");
+            int num1 = in.nextInt();
+            int num2 = in.nextInt();
+            int num3 = in.nextInt();
+            int num4 = in.nextInt();
+
+            System.out.print("НОД " + num1 + " " + num2 + " " + num3 + " " + num4 + " = ");
+            System.out.println(gcd4(num1, num2, num3, num4));
+        }
+    }
+
+    //НОД
+    public static long gcd(long num1, long num2) {
+        return num2 == 0 ? num1 : gcd(num2, num1 % num2);
+    }
+
+    public static long gcd4(long num1, long num2, long num3, long num4) {
+        return gcd(gcd(num1, num2), gcd(num3, num4));
+    }
+}
