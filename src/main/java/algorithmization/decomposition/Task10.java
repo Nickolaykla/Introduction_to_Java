ecac2f0eee562dbf80ff67693c34d0f72c3995ed
Nickolaykla <nickolaykla@gmail.com>
2022-08-31 15:25:02 +0300

    task10 decomp added

---
+++ src/main/java/algorithmization/decomposition/Task10.java
@@ -0,0 +1,24 @@
+package algorithmization.decomposition;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+Дано натуральное число N. Написать метод(методы) для формирования массива,
+элементами которого являются цифры числа N.
+ */
+public class Task10 {
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Введите число: ");
+            long N = in.nextLong();
+            if (N < 1) throw new IllegalArgumentException();
+
+            System.out.println(Arrays.toString(getArray(N)));
+        }
+    }
+
+    public static char[] getArray(long n) {
+        return String.valueOf(n).toCharArray();
+    }
+}
