69f019ad72f3079dac48163aef42ace79293c557
Nickolaykla <nickolaykla@gmail.com>
2022-09-04 12:25:41 +0300

    added 1 task classes

---
+++ src/main/java/programming_with_classes/task1/Test1.java
@@ -0,0 +1,62 @@
+package programming_with_classes.task1;
+
+/*
+Создайте класс Test1 с двумя переменными. Добавьте метод вывода на экран и методы изменения этих
+переменных. Добавьте метод, который находит сумму значений этих переменных, и метод, который
+находит наибольшее значение из этих двух переменных.
+* */
+public class Test1 {
+    private int a = 10;
+    private int b = 20;
+
+    public static void main(String[] args) {
+        Test1 test1 = new Test1();
+        System.out.println("Значения a и b:");
+        System.out.println(test1);
+
+        System.out.println("Изменение значений a и b:");
+        test1.setA(20);
+        test1.setB(30);
+        System.out.println(test1);
+
+        System.out.println("Сумма значений a и b:");
+        int sum = test1.sum(test1.getA(), test1.getB());
+        System.out.println(sum);
+
+        System.out.println("Наибольшее значение:");
+        int max = test1.max(test1.getA(), test1.getB());
+        System.out.println(max);
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int max(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    @Override
+    public String toString() {
+        return "Test1{" +
+                "a=" + a +
+                ", b=" + b +
+                '}';
+    }
+}
--- src/main/java/strings_and_basics_of_text_proccessing/work_with_regExpressions/Task2.java
+++ src/main/java/strings_and_basics_of_text_proccessing/work_with_regExpressions/Task2.java
@@ -1,11 +1,87 @@
 package strings_and_basics_of_text_proccessing.work_with_regExpressions;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /*
 Напишите анализатор, позволяющий последовательно возвращать содержимое узлов xml-документа и его
 тип (открывающий тег, закрывающий тег, содержимое тега, тег без тела).
 * */
 public class Task2 {
     public static void main(String[] args) {
-        System.out.println('A' + 'B');
+        String xml = "<notes>" + "\n\t" +
+                        "<note id = '1'>" + "\n\t\t" +
+                             "<to>Вася</to>" + "\n\t\t" +
+                             "<from>Света</from>" + "\n\t\t" +
+                             "<heading>Напоминание</heading>" + "\n\t\t" +
+                             "<body>Позвони мне завтра!</body>" + "\n\t" +
+                         "</note>" + "\n\t\t" +
+                         "<note id = '2'>" + "\n\t\t" +
+                             "<to>Петя</to>" + "\n\t\t" +
+                             "<from>Маша</from>" + "\n\t\t" +
+                             "<heading>Важное напоминание</heading>" + "\n\t\t" +
+                             "<body/>" + "\n\t" +
+                        "</note>" + "\n" +
+                "</notes>";
+        System.out.println(numOfOpenTegs(xml));
+        System.out.println(numOfClosingTegs(xml));
+        parseXML(xml);
+
+    }
+
+    public static int numOfOpenTegs(String s) {
+        Pattern pattern = Pattern.compile("<[^/].+?[^/]>");
+        Matcher matcher = pattern.matcher(s);
+        int count = 0;
+        while (matcher.find()) count++;
+        return count;
+    }
+
+    public static int numOfClosingTegs(String s) {
+        Pattern pattern = Pattern.compile("</.+?>");
+        Matcher matcher = pattern.matcher(s);
+        int count = 0;
+        while (matcher.find()) count++;
+        return count;
+    }
+
+    public static boolean checkTagsCount(String s) {
+        return numOfOpenTegs(s) == numOfClosingTegs(s);
+    }
+    public static String getCloseTag(String s) {
+        if(s.isEmpty()) return null;
+        Pattern pattern = Pattern.compile("\\b\\w+?\\b");
+        Matcher matcher = pattern.matcher(s);
+
+        if(matcher.find()) {
+            return "</" + matcher.group() + ">";
+        } else return " ";
+    }
+
+    public static void parseXML(String s) {
+        Pattern pattern = Pattern.compile("<[^/].+?>");
+        Matcher matcher = pattern.matcher(s);
+        String open;
+        String close;
+        int start;
+        int end;
+
+        while (matcher.find()) {
+           open = matcher.group();
+           close = getCloseTag(open);
+
+           start = s.indexOf(open);
+           end = s.indexOf(close);
+
+           if (end != -1) {
+               end += close.length();
+               String result = s.substring(start, end);
+               System.out.println(result);
+
+               start += open.length();
+           }
+
+
+        }
     }
 }
